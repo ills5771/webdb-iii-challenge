38c354003ecaf9186faae1dc75bfde7ee9d7c222
ills5771 <ilyayelizarov1@gmail.com>
2019-03-27 12:32:51 -0700

    created knex file and migrations, created cohorts table

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const helmet = require("helmet");
 const knex = require("knex");
 
+const knexConfig = {
+  client: "sqlite3",
+  connection: {
+    filename: "./data/cohorts.db3"
+  },
+  useNullAsDefault: true // needed for sqlite
+};
+
 const db = knex(knexConfig);
 
 const server = express();
