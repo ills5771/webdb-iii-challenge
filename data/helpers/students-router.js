6841236c4fdcf5359efb4d56c709e32801f9268a
ills5771 <ilyayelizarov1@gmail.com>
2019-03-27 19:00:42 -0700

    added put and delete enpoints for students

--- data/helpers/students-router.js
+++ data/helpers/students-router.js
@@ -27,5 +27,47 @@ router.get("/:id", async (req, res) => {
     res.status(500).json(error);
   }
 });
+router.post("/", async (req, res) => {
+  try {
+    const [id] = await db("students").insert(req.body);
+    const student = await db("students")
+      .where({ id })
+      .first();
+    res.status(201).json(student);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "We ran into an error adding the student" });
+  }
+});
+router.put("/:id", async (req, res) => {
+  try {
+    const count = await db("students")
+      .where({ id: req.params.id })
+      .update(req.body);
+
+    if (count > 0) {
+      const student = await db("students")
+        .where({ id: req.params.id })
+        .first();
+      res.status(200).json(student);
+    } else {
+      res.status(404).json({ message: "That student wasn't found" });
+    }
+  } catch (error) {}
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const count = await db("students")
+      .where({ id: req.params.id })
+      .del();
+    if (count > 0) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: "That student wasn't found" });
+    }
+  } catch (error) {}
+});
 
 module.exports = router;
