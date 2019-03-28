9f9d41b38069e37830450575452d537ad00a03e3
ills5771 <ilyayelizarov1@gmail.com>
2019-03-27 18:38:59 -0700

    added get request for retrieving each student by id

--- data/helpers/students-router.js
+++ data/helpers/students-router.js
@@ -12,4 +12,20 @@ router.get("/", async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+router.get("/:id", async (req, res) => {
+  try {
+    const student = await db("students")
+      .where({ id: req.params.id })
+      .first();
+    if (student) {
+      res.status(201).json(student);
+    } else {
+      res.status(404).json({ message: "That student does not exist" });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
--- migrations/20190327152203_students_table.js
+++ migrations/20190327152203_students_table.js
@@ -1,4 +1,4 @@
-exports.up = function(knex, Promise) {
+exports.up = function(knex) {
   return (
     knex.schema.createTable("students"),
     function(tbl) {
@@ -16,6 +16,6 @@ exports.up = function(knex, Promise) {
   );
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function(knex) {
   return knex.schema.dropTableIfExists("students");
 };
